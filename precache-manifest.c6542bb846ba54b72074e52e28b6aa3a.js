64455719cfade84dc7b067680957f623b4417ee2
Presley <presley@pop-os.localdomain>
2020-02-19 21:11:56 +0800

    Updates

---
+++ precache-manifest.c6542bb846ba54b72074e52e28b6aa3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83e77c75369b9ab4fb41eddb41aaa0eb",
+    "url": "/react-tube/index.html"
+  },
+  {
+    "revision": "c0d134e5f600fadfe021",
+    "url": "/react-tube/static/js/2.b2c7b8ca.chunk.js"
+  },
+  {
+    "revision": "06e21e4f9dc8f5faabfd9483e01e25f4",
+    "url": "/react-tube/static/js/2.b2c7b8ca.chunk.js.LICENSE"
+  },
+  {
+    "revision": "119e6994240d1e5d8d66",
+    "url": "/react-tube/static/js/main.88c85e09.chunk.js"
+  },
+  {
+    "revision": "3fef26a736726e0a8f37",
+    "url": "/react-tube/static/js/runtime-main.dc91622e.js"
+  }
+]);
